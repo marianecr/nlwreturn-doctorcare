7f60ea791538fdc5215938fd27678a21212fc2f8
Mariane Rodrigues <104990470+marianecr@users.noreply.github.com>
2022-05-11 17:39:49 -0300

    Upload do projeto.

---
+++ main.js
@@ -0,0 +1,82 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll(){
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2
+
+    // quais dados são necessários?
+    // Verificando se a seção passou da linha 
+    const sectionTop = section.offsetTop
+    const sectionHeigth = section.offsetHeigth
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    // Verifica se a base está abaixo da linha alvo
+    const sectionEndsAt = sectionTop + sectionHeigth
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    // Limites da seção
+    const sectionBoundaries = 
+          sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    // Identificando qual elemento do menu deve ser ativado
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href *= ${sectionId}]`)
+
+    menuElement.classList.remove('active')
+
+    if (sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+}
+
+function showNavOnScroll(){
+    if (scrollY > 0){
+        navigation.classList.add('scroll')  
+
+    }
+    else{
+        navigation.classList.remove('scroll')  
+
+    }
+}
+
+function showBackToTopButtonOnScroll(){
+    if (scrollY > 500){
+        backToTopButton.classList.add('show')  
+    }
+    else{
+        backToTopButton.classList.remove('show')  
+
+    }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .cards`)
